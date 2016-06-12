630e515d4b6194cc8118cf64824f329fe4c80fd6
Guy Segal <guy@soluto.com>
2016-06-12 17:54:30 +0300

    use RemoteCallInvoker instead of RpcListener

--- src/main/java/soluto/congo/android/intent/IntentRemoteCallInvoker.java
+++ src/main/java/soluto/congo/android/intent/IntentRemoteCallInvoker.java
@@ -6,38 +6,28 @@ import android.content.IntentFilter;
 
 import com.google.gson.Gson;
 
-import java.util.UUID;
-
 import rx.Notification;
 import rx.Observable;
 import rx.functions.Action0;
 import rx.functions.Func1;
 import rxutils.BroadcastObservable;
 import soluto.congo.core.RemoteCall;
-import soluto.congo.core.RpcClient;
+import soluto.congo.core.RemoteCallInvoker;
 
-public class IntentRemoteCallInvoker implements RpcClient {
+public class IntentRemoteCallInvoker implements RemoteCallInvoker {
     private static final String INTENT_DATA_NAME = "data";
     private final Context context;
-    private final String serviceName;
     private final String intentPermission;
     private final String intentPrefix;
 
-    public IntentRemoteCallInvoker(Context context, String serviceName, String intentPrefix, String intentPermission) {
+    public IntentRemoteCallInvoker(Context context, String intentPrefix, String intentPermission) {
         this.context = context;
-        this.serviceName = serviceName;
         this.intentPermission = intentPermission;
         this.intentPrefix = intentPrefix;
     }
 
     @Override
-    public <TResult> Observable<TResult> invoke(String methodName, Class<TResult> tResultClass, Object... args) {
-        RemoteCall remoteCall = new RemoteCall();
-        remoteCall.service = serviceName;
-        remoteCall.method = methodName;
-        remoteCall.args = args;
-        remoteCall.correlationId = UUID.randomUUID().toString();
-
+    public <TResult> Observable<TResult> invoke(RemoteCall remoteCall, Class<TResult> tResultClass) {
         final Intent intent = new Intent(intentPrefix + "_RemoteCall");
         intent.putExtra(INTENT_DATA_NAME, new Gson().toJson(remoteCall));
 
